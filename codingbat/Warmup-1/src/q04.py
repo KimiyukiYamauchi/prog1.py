849fdf20b33e07ee61313ed867425d250d681241
Kimiyuki Yamauchi <kimiyuki.yamauchi.0313@gmail.com>
2025-06-29 12:31:24 +0900

    コメントを日本語化(q04.py)

--- codingbat/Warmup-1/src/q04.py
+++ codingbat/Warmup-1/src/q04.py
@@ -1,8 +1,15 @@
-# Given an int n, return the absolute difference between n and 21,
-# except return double the absolute difference if n is over 21.
-# diff21(19) → 2
-# diff21(10) → 11
-# diff21(21) → 0
+# 1つの引数(整数)を受け取り21との差を返す、但し、引数が21より
+# 大きければ差の2倍を返す関数
+# 例：
+# 　diff21(19) → 2
+# 　diff21(10) → 11
+# 　diff21(21) → 0
+# 
+# @param int n 整数
+# 
+# @return 1つの引数(整数)を受け取り21との差を返す、ただし、引数が21より
+# 　　　　大きければ差の2倍を返す
+#
 
 def diff21(n):
     return -1
