833b716c47e9c7a811a4f548c8ab51db06e8474a
Kimiyuki Yamauchi <kimiyuki.yamauchi.0313@gmail.com>
2025-06-29 09:24:25 +0900

    関数の説明を日本語にする

--- codingbat/Warmup-1/src/q01.py
+++ codingbat/Warmup-1/src/q01.py
@@ -1,10 +1,20 @@
-# The parameter weekday is True if it is a weekday, 
-# and the parameter vacation is True if we are on vacation. 
-# We sleep in if it is not a weekday or we're on vacation. 
-# Return True if we sleep in.
-# sleep_in(False, False) → True
-# sleep_in(True, False) → False
-# sleep_in(False, True) → True
+# sleep_in: 寝坊してもよいかを判定する関数
+#  引数の weekday は、平日であればTrue で、引数の vacation は、
+#  休暇中であればTrue です。平日ではないか、休暇中であれば寝坊します。
+#  寝坊する場合はTrue を返します。
+#  
+#  weekday 平日であればTrue、土日はFalse
+#  vacation 祝日はTrue、そうでなければFalse
+#  
+#  weekdayがFalseまたはvacationがTrueなら遅起きしてよい
+#  
+#  @return 寝坊してもよければTrue、そうでなければFalse
+#  
+#  
+#  例：
+#  　sleep_in(False, False) → True
+#  　sleep_in(True, False) → False
+#  　sleep_in(False,True) → True
 
 def sleep_in(weekday, vacation):
     return False
