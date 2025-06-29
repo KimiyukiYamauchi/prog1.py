f57401cc359f1034097a2ca0a1e0313d06640d1a
Kimiyuki Yamauchi <kimiyuki.yamauchi.0313@gmail.com>
2025-06-29 09:51:43 +0900

    関数の説明を日本語化

--- codingbat/Warmup-1/src/q02.py
+++ codingbat/Warmup-1/src/q02.py
@@ -1,9 +1,20 @@
-# We have two monkeys, a and b, and the parameters a_smile and b_smile indicate if each is smiling.
-# We are in trouble if they are both smiling or if neither of them is smiling.
-# Return True if we are in trouble.
-# monkey_trouble(True, True) → True
-# monkey_trouble(False, False) → True
-# monkey_trouble(True, False) → False
+# monkey_trouble: 2匹のサルがトラブルを起こすかをチェックする関数
+# ２匹のサルがトラブルを起こすかをチェックする関数
+# 2匹のサル、a と b がおり、パラメーター a_smile と b_smile は
+# それぞれが笑っているかどうかを示します。両方が笑っている場合、
+# またはどちらも笑っていない場合、2匹のサルはトラブルを起こします。
+# 困っている場合は True を返します。
+# 
+# a_smile aのサルが笑っていればTrue、そうでなければFalse
+# b_smile bのサルが笑っていればTrue、そうでなければFalse
+# 
+# トラブルを起こす場合はTrue、そうでなければFalse
+# 
+# ２匹のサルが両方とも笑っているか、両方とも笑っていなければトラブルを起こす
+# 例：
+# 　monkeyTrouble(True, True) → True
+# 　monkeyTrouble(False, False) → True
+# 　monkeyTrouble(True, False) → False
 
 def monkey_trouble(a_smile, b_smile):
     return False
