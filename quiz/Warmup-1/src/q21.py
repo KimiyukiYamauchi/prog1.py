c05caf0d5b713bca46d79d11f23c01a55e22b07a
Kimiyuki Yamauchi <kimiyuki.yamauchi.0313@gmail.com>
2025-07-02 20:46:16 +0900

    q21追加

---
+++ quiz/Warmup-1/src/q21.py
@@ -0,0 +1,18 @@
+# 引数(文字列)のindexが1の位置から"del"があればそれを削除して返す
+# なければ引数(文字列)をそのまま返す関数
+# 例：
+# 　del_del("adelbc") → "abc"
+# 　del_del("adelHello") → "aHello"
+# 　del_del("adedbc") → "adedbc"
+# 
+# @param string str 文字列
+# 
+# @return 引数(文字列)のindexが1の位置から"del"があればそれを削除して返す
+# 　　　　なければ引数(文字列)をそのまま返す
+# 
+#/
+
+def del_del(str):
+    if len(str) >= 4 and str[1:4] == "del":
+        return str[0] + str[4:]
+    return str
