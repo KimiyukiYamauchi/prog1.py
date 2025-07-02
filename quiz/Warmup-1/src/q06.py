2b56c40087f4841faee5a20c17059d11e2885021
itc-s25001 <s25001@std.it-college.ac.jp>
2025-07-02 07:18:55 +0000

    q06.py

--- quiz/Warmup-1/src/q06.py
+++ quiz/Warmup-1/src/q06.py
@@ -15,4 +15,10 @@
 #         それ以外はFalse
 # 
 def makes10(a, b):
+    if a==10 or b==10:
+        return True
+    elif a+b==10:
+        return True
+    else:
+        return False
     return a == 10 or b == 10 or (a + b) == 10
