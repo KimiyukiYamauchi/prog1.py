206dd94770c4efbd78ace5309c8fed55795d5f6a
Kimiyuki Yamauchi <kimiyuki.yamauchi.0313@gmail.com>
2025-07-07 23:21:44 +0900

    ひな形の修正

--- quiz/Warmup-2/src/q06.py
+++ quiz/Warmup-2/src/q06.py
@@ -1,4 +1,3 @@
-/**
 # 与えられたint型の配列で、要素が9の数を返す関数
 # 例：
 # 　array_count9([1, 2, 9]) → 1
@@ -8,8 +7,6 @@
 # @param array nums int型の配列
 # 
 # @return 上記の条件での数を返す
-# 
-#/
 
 def array_count9(nums):
     return -1
