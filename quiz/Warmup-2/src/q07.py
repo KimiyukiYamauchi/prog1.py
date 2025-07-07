097db6c08e98d010a5cc64ddaec9b4f894b55518
Kimiyuki Yamauchi <kimiyuki.yamauchi.0313@gmail.com>
2025-07-07 22:20:02 +0900

    q07追加

---
+++ quiz/Warmup-2/src/q07.py
@@ -0,0 +1,14 @@
+# 与えられたint型の配列の4番目までの要素に9があればtrueを返す
+# 但し、配列の要素数は4に満たない場合がある
+# 例：
+# 　array_front9([1, 2, 9, 3, 4]) → true
+# 　array_front9([1, 2, 3, 4, 9]) → false
+# 　array_front9([1, 2, 3, 4, 5]) → false
+# 
+# @param array $nums int型の配列
+# 
+# @return 上記の条件での数を返す
+# 
+
+def array_front9(nums):
+    return -1
