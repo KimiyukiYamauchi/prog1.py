9e176d243fd8e908a1bf91adf3eb89f93ce8caa0
Kimiyuki Yamauchi <kimiyuki.yamauchi.0313@gmail.com>
2025-07-07 23:25:55 +0900

    実装を削除

--- quiz/Warmup-2/src/q09.py
+++ quiz/Warmup-2/src/q09.py
@@ -12,9 +12,4 @@
 # @return 上記の条件が成り立つ個数を返す
 
 def string_match(a, b):
-    count = 0
-    max_len = min(len(a), len(b))
-    for i in range(max_len - 1):
-        if a[i:i+2] == b[i:i+2]:
-            count += 1
-    return count
+    return -1
