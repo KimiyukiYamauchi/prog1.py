4deb69d7fb1e565df5952c1ca997585506e53b4e
Kimiyuki Yamauchi <kimiyuki.yamauchi.0313@gmail.com>
2025-07-09 22:11:36 +0900

    q12追加

---
+++ quiz/List-1/src/q12.py
@@ -0,0 +1,14 @@
+# 引数で与えられた要素数2のint型の配列に2または3が含まれている場合Trueを返す
+# それ以外はFalseを返す
+
+# 例：
+# 　has23([2, 5]) → True
+# 　has23([4, 3]) → True
+# 　has23([4, 5]) → False
+# 
+# @param int [] nums int型の配列
+# 
+# @return bool True or False
+
+def has23(nums):
+    return 2 in nums or 3 in nums
