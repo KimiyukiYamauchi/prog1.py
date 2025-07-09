863ea1d1ef46414f2ad46e0ef3fde48ab6096b02
Kimiyuki Yamauchi <kimiyuki.yamauchi.0313@gmail.com>
2025-07-09 21:18:52 +0900

    q05追加

---
+++ quiz/List-1/src/q05.py
@@ -0,0 +1,13 @@
+# 要素数が3のint型の配列を引数で受け取り、その要素の合計を
+# 返す関数
+# 例：
+# 　sum3([1, 2, 3]) → 6
+# 　sum3([5, 11, 2]) → 18
+# 　sum3([7, 0, 0]) → 7
+# 
+# @param int [] nums int型の配列
+# 
+# @return 要素の合計値
+
+def sum3(nums):
+    return sum(nums)
