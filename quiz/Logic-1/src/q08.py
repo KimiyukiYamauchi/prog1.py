a0404f0b313f394e825d0acda2d4701c5e3b7c5c
Kimiyuki Yamauchi <kimiyuki.yamauchi.0313@gmail.com>
2025-07-16 21:44:16 +0900

    q08を追加

---
+++ quiz/Logic-1/src/q08.py
@@ -0,0 +1,11 @@
+'''
+数値 n が与えられたとき、n が 1 から 10 までの範囲にあれば真を返す。
+ただし、outside_modeがTrueの場合は、数値が1以下または10以上の場合にTrueを返す。
+
+n 整数
+outside_mode TrueまたはFalse
+
+return 上記の条件に従ってTrueまたはFalseを返す
+'''
+def in1to10(n, outside_mode):
+    return False
