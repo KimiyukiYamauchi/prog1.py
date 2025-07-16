31ade7c3e6ce3f78bfea5833b048b98cef94bd91
Kimiyuki Yamauchi <kimiyuki.yamauchi.0313@gmail.com>
2025-07-16 21:20:04 +0900

    q05追加

---
+++ quiz/Logic-1/src/q05.py
@@ -0,0 +1,15 @@
+'''
+2つのint、aとbが与えられたら、それらの和を返す。
+ただし、10～19の範囲の和は禁止されているので、
+その場合は20を返せばよい。
+
+a 整数
+b 整数
+
+@return 上記の条件で、aとbの和を返す
+'''
+def sorta_sum(a, b):
+    total = a + b
+    if 10 <= total <= 19:
+        return 20
+    return total
