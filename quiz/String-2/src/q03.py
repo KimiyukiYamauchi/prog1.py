859b54206d8e9729630e1ef5ebb7b25e335a7204
Kimiyuki Yamauchi <kimiyuki.yamauchi.0313@gmail.com>
2025-07-20 21:41:22 +0900

    q03追加

---
+++ quiz/String-2/src/q03.py
@@ -0,0 +1,13 @@
+'''
+文字列 「cat 」と 「dog 」が、与えられた文字列の中で同じ回数出現したらTrueを返す。
+
+s: 文字列
+
+return: True または False
+
+cat_dog('catdog') → True
+cat_dog('catcat') → False
+cat_dog('1cat1cadodog') → True
+'''
+def cat_dog(s):
+    return False
