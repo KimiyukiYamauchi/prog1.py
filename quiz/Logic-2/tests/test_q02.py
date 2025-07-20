6dd7ee3e559a8c05791015fa17a03cd841ce5ab5
Kimiyuki Yamauchi <kimiyuki.yamauchi.0313@gmail.com>
2025-07-20 17:47:09 +0900

    q02追加

---
+++ quiz/Logic-2/tests/test_q02.py
@@ -0,0 +1,16 @@
+import pytest
+from q02 import lone_sum
+
+@pytest.mark.parametrize("a, b, c, expected", [
+    (1, 2, 3, 6),
+    (3, 2, 3, 2),
+    (3, 3, 3, 0),
+    (9, 2, 2, 9),
+    (2, 2, 9, 9),
+    (2, 9, 2, 9),
+    (2, 9, 3, 14),
+    (4, 2, 3, 9),
+    (1, 3, 1, 3),
+])
+def test_lone_sum(a, b, c, expected):
+    assert lone_sum(a, b, c) == expected
