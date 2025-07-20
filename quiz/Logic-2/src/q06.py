ff0fd456c36e56d4497d445e54d49db084d50119
Kimiyuki Yamauchi <kimiyuki.yamauchi.0313@gmail.com>
2025-07-20 18:24:31 +0900

    q06追加

---
+++ quiz/Logic-2/src/q06.py
@@ -0,0 +1,16 @@
+```
+a b cの3つのintが与えられたとき、bまたはcの一方が 「近い」(aとの差が最大でも1)、
+他方が 「遠い」(他の両方の値との差が2以上)ならTrueを返す。
+
+a: 整数
+b: 整数
+c: 整数
+
+return: 真偽値(True/False)
+
+close_far(1, 2, 10) → True
+close_far(1, 2, 3) → False
+close_far(4, 1, 3) → True
+```
+def close_far(a, b, c):
+    return False
---
+++ quiz/Logic-2/tests/test_q06.py
@@ -0,0 +1,18 @@
+import pytest
+from q06 import close_far
+
+@pytest.mark.parametrize("a, b, c, expected", [
+    (1, 2, 10, True),
+    (1, 2, 3, False),
+    (4, 1, 3, True),
+    (4, 5, 3, False),
+    (4, 3, 5, False),
+    (-1, 10, 0, True),
+    (0, -1, 10, True),
+    (10, 10, 8, False),
+    (10, 8, 9, False),
+    (8, 9, 10, False),
+    (8, 6, 9, True),
+])
+def test_close_far(a, b, c, expected):
+    assert close_far(a, b, c) == expected
