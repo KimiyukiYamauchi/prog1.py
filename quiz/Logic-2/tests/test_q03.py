d61c001aa7bd3732b7d0371c5f589f76e6195e5b
Kimiyuki Yamauchi <kimiyuki.yamauchi.0313@gmail.com>
2025-07-20 17:52:59 +0900

    q03追加

---
+++ quiz/Logic-2/tests/test_q03.py
@@ -0,0 +1,19 @@
+import pytest
+from q03 import lucky_sum
+
+@pytest.mark.parametrize("a, b, c, expected", [
+    (1, 2, 3, 6),
+    (1, 2, 13, 3),
+    (1, 13, 3, 1),
+    (1, 13, 13, 1),
+    (6, 5, 2, 13),
+    (13, 2, 3, 0),
+    (13, 2, 13, 0),
+    (13, 13, 2, 0),
+    (9, 4, 13, 13),
+    (8, 13, 2, 8),
+    (7, 2, 1, 10),
+    (3, 3, 13, 6),
+])
+def test_lucky_sum(a, b, c, expected):
+    assert lucky_sum(a, b, c) == expected
