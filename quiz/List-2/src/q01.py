102c30ee0a8ba9fed88bd5aabaadd04a647e6666
itc-n25007 <n25007@std.it-college.ac.jp>
2025-07-30 07:31:32 +0000

    練習問題

--- quiz/List-2/src/q01.py
+++ quiz/List-2/src/q01.py
@@ -1,9 +1,9 @@
-'''
-与えられた配列に含まれる偶数の int の数を返す。
+#'''
+#与えられた配列に含まれる偶数の int の数を返す。
 
-nums: int[] - 整数のリスト
-戻り値: int - 偶数の数
-'''
+#nums: int[] - 整数のリスト
+#戻り値: int - 偶数の数
+#'''
 
 def count_evens(nums):
-    return -1
+    return len([num for num in nums if num % 2 == 0])
--- quiz/List-2/src/q06.py
+++ quiz/List-2/src/q06.py
@@ -1,8 +1,11 @@
-'''
-int の配列が与えられたとき、配列のどこかに 2 の隣に 2 があればTrueを返す。
-
-nums: 数字のリスト
-return: 2 の隣に 2 がある場合は True、そうでなければ False
-'''
+#'''
+#int の配列が与えられたとき、配列のどこかに 2 の隣に 2 があればTrueを返す。
+#
+#nums: 数字のリスト
+#return: 2 の隣に 2 がある場合は True、そうでなければ False
+#'''
 def has22(nums):
+    for i in range(len(nums) - 1):
+        if nums[i] == 2 and nums [i + 1] == 2:
+            return True
     return False
