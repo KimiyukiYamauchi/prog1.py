a7b110254147060c442e5682c32cf12c134707f2
Kimiyuki Yamauchi <kimiyuki.yamauchi.0313@gmail.com>
2026-06-20 10:27:22 +0900

    sleep_in関数の仕様の説明を修正

--- quiz/01.Warmup-1/src/q01.py
+++ quiz/01.Warmup-1/src/q01.py
@@ -1,10 +1,11 @@
 # sleep_in: 寝坊してもよいかを判定する関数
-#  引数の weekday は、平日であればTrue で、引数の vacation は、
-#  休暇中であればTrue です。平日ではないか、休暇中であれば寝坊します。
-#  寝坊する場合はTrue を返します。
+# 引数の weekday は、平日であればTrue で、週末でのあればFalse が設定されます。
+# 引数の vacation は、休暇中であればTrueで、 そうでなければFalse が設定されます。
+# 平日ではないか、休暇中であれば寝坊してもOKです。
+# 寝坊してもOKの場合はTrue を返します。
 #  
 #  weekday 平日であればTrue、土日はFalse
-#  vacation 祝日はTrue、そうでなければFalse
+#  vacation休暇中はTrue、そうでなければFalse
 #  
 #  weekdayがFalseまたはvacationがTrueなら遅起きしてよい
 #  
